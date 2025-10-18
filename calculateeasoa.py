a43777a96bf9b7df7e2888f6ca5ac69e7070f5f7
leangvakhim <Vakhiml60@gmail.com>
2025-10-18 21:33:02 +0700

    init

--- calculateeasoa.py
+++ calculateeasoa.py
@@ -1,7 +1,8 @@
-## Equation 1
 import math
+import random
+import numpy as np
 
-
+## Equation 1
 def single_sensor_prob(sensor_position, point_position, sensor_radius):
 
     distance = math.dist(sensor_position, point_position)
@@ -18,3 +19,43 @@ def total_coverage_prob(sensors, point, sensor_radius):
         product_failure *= failure_prob
         total_prob = 1 - product_failure
     return total_prob
+
+# Equation 2
+def total_coverage(total_sum_coverage, total_monitoring_point):
+    return total_sum_coverage / total_monitoring_point
+
+# Equation 3
+def reverse_elite_selection(x, x_min, x_max):
+    return x_max + x_min - x
+
+# Equation 4
+def update_reverse_elite(x, x_prime):
+    if fitness_value(x_prime) < fitness_value(x):
+        return x_prime
+    else:
+        return x
+
+# Equation 5
+def brightness_driven_perturbation(x_i, x_j, beta, alpha=0.5, gamma=0.7):
+    distance_square = (x_i - x_j) ** 2
+    # theta = np.random.randn(*x_i.shape) * 0.1
+    theta = 0.1
+    perturbation = beta * math.exp(-gamma * distance_square)
+    random_term = alpha * theta
+    return x_i + perturbation + random_term
+
+# Equation 6
+def update_attraction_coefficient(beta_initial, k, k_max):
+    beta = beta_initial * (1 - (k / k_max))
+    return beta
+
+# Equation 7
+def dynamic_warning_update(x, x_best, delta):
+    r = random.random()
+    x_new = x + delta * (r * x_best - x)
+    return x_new
+
+# Equation 8
+def fitness_value(coverage, energy, dvar, omega1, omega2, omega3):
+    fitness = (omega1 * coverage) - (omega2 * dvar) - (omega3 * energy)
+    return fitness
